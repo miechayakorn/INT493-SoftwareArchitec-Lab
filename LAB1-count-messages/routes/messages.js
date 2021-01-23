d52e122d2867e285907133b016d9664cf0d8caaf
Mie Chayakorn <miechayakorn@gmail.com>
2021-01-23 22:02:16 +0700

    [Edit] fix bug

--- LAB1-count-messages/routes/messages.js
+++ LAB1-count-messages/routes/messages.js
@@ -3,30 +3,31 @@ var router = express.Router();
 
 const messages = require('../public/temp/massage.json')
 
-
 router.get('/', function (req, res, next) {
     res.json(messages)
 });
 
 router.post('/', function (req, res, next) {
-    let found = false
-    messages.data.forEach(data => {
-        if (data.text == req.body.text) {
-            data.count = data.count + 1
-            console.log("============= +1")
-            found = true
+    if (req.body.text) {
+        let found = false
+        messages.data.forEach(data => {
+            if (data.text == req.body.text) {
+                data.count = data.count + 1
+                found = true
+                res.status(200).json(messages)
+            }
+        });
+        if (found == false) {
+            const add = {
+                "text": req.body.text,
+                "count": 1
+            }
+            messages.data.push(add)
             res.status(201).json(messages)
         }
-    });
-    if (found == false) {
-        const add = {
-            "text": req.body.text,
-            "count": 1
-        }
-        messages.data.push(add)
-        console.log("============= create!!")
-        console.log("ADDED!!!")
-        res.status(201).json(messages)
+    } else {
+        console.log("ldkfjlk")
+        res.status(400).json("โปรดใส่ข้อมูลที่ถูกต้อง!!")
     }
 });
 
